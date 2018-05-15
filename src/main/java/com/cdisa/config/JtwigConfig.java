5f7345e02ae616daf1b01a1484931427e4d9e60f
Lucas Strübi <lucas.strubi@gmail.com>
2018-05-15 11:52:21 +0200

    Assets configuration
    Untrack cdisa_football.iml

--- src/main/java/com/cdisa/config/JtwigConfig.java
+++ src/main/java/com/cdisa/config/JtwigConfig.java
@@ -1,22 +1,37 @@
 package com.cdisa.config;
 
 import org.jtwig.environment.EnvironmentConfigurationBuilder;
-import org.jtwig.render.RenderExtension;
 import org.jtwig.spring.JtwigViewResolver;
+import org.jtwig.spring.asset.SpringAssetExtension;
+import org.jtwig.spring.asset.resolver.AssetResolver;
+import org.jtwig.spring.asset.resolver.BaseAssetResolver;
 import org.jtwig.spring.boot.config.JtwigViewResolverConfigurer;
 import org.jtwig.web.servlet.JtwigRenderer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
 @EnableWebMvc
-public class JtwigConfig implements JtwigViewResolverConfigurer {
-
+@Configuration
+public class JtwigConfig extends WebMvcConfigurerAdapter implements JtwigViewResolverConfigurer {
     @Override
     public void configure(JtwigViewResolver viewResolver) {
         viewResolver.setRenderer(new JtwigRenderer(EnvironmentConfigurationBuilder.configuration()
-                .extensions().add(new RenderExtension()).and()
+                .extensions().add(new SpringAssetExtension()).and()
                 .build()));
     }
 
+    @Bean
+    public AssetResolver assetResolver () {
+        BaseAssetResolver assetResolver = new BaseAssetResolver();
+        assetResolver.setPrefix("public");
+        return assetResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("public/**").addResourceLocations("classpath:public/");
+    }
 }
